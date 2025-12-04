e80f353f3fd5c0fae009fda59cf83e375105c589
yusufserdarr <yusuf.sdrd@gmail.com>
2025-12-04 12:29:23 +0300

    Initial Commit

---
+++ suİzim/suI_zimApp.swift
@@ -0,0 +1,32 @@
+//
+//  suI_zimApp.swift
+//  suİzim
+//
+//  Created by Yusuf Serdaroğlu on 4.12.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct suI_zimApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ suİzimTests/suI_zimTests.swift
@@ -0,0 +1,16 @@
+//
+//  suI_zimTests.swift
+//  suİzimTests
+//
+//  Created by Yusuf Serdaroğlu on 4.12.2025.
+//
+
+import Testing
+
+struct suI_zimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
